9ac40b1365b0b551965b7e796f6a8c5b2ddf07ab
Ven4o <vencislav-georgiev@abv.bg>
2026-02-16 10:32:55 +0200

    Added ListActonsApp

---
+++ Venci1/ListOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ListOperations
+{
+
+    public static List<int> InsertElement(List<int> list, int index, int element)
+    {
+        list.Insert(index, element);
+        return list;
+    }
+
+    public static List<int> DeleteElement(List<int> list, int element)
+    {
+        list.Remove(element);
+        return list;
+    }
+
+
+    public static void PrintList(List<int> list)
+    {
+        Console.WriteLine("Current list: ");
+        foreach (var item in list)
+        {
+            Console.Write(item + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Venci1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        List<int> myList = new List<int> { 1, 2, 3, 4, 5 };
+
+
+        myList = Venci.InsertElement(myList, 2, 10);
+        Venci.PrintList(myList);
+
+
+        myList = Venci.DeleteElement(myList, 3);
+        Venci.PrintList(myList);
+    }
+}
